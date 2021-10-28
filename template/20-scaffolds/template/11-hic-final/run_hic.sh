c077f3d242e6ca9d7ed699ff1114c5f8d9105c8a
yoshihikosuzuki <ys.neoteny@gmail.com>
2021-10-28 15:38:03 +0900

    separate hic into early and final

--- template/20-scaffolds/template/11-hic-final/run_hic.sh
+++ template/20-scaffolds/template/11-hic-final/run_hic.sh
@@ -32,7 +32,7 @@ mkdir -p references && cd references && ln -sf ../${SCAFS%.*}.* . && cd ..
 
 SCAFS=references/${SCAFS}
 CHROM_SIZES=${SCAFS%.*}.chrom_sizes
-ASSEMBLY=${SCAFS%.*}.assembly
+# ASSEMBLY=${SCAFS%.*}.assembly
 
 # 3d-dna-fasta2assembly ${SCAFS} >${ASSEMBLY}
 # awk 'NF == 3 {print substr($1,2) "\t" $3}' ${ASSEMBLY} >${CHROM_SIZES}
@@ -48,10 +48,7 @@ else
     cd ..
     JUICER_S_OPT="-s ${ENZYME_NAME}"
 fi
-./scripts/juicer.sh -t ${N_THREADS} -S early -z ${SCAFS} -p ${CHROM_SIZES} -g ${OUT_PREFIX} ${JUICER_S_OPT}
-mkdir -p hic && cd hic &&
-    3d-dna-run-assembly-visualizer ../${ASSEMBLY} ../aligned/merged_nodups.txt
-cd ..
+./scripts/juicer.sh -t ${N_THREADS} -z ${SCAFS} -p ${CHROM_SIZES} -g ${OUT_PREFIX} ${JUICER_S_OPT}
 
 if [ "$AUTO_DEL" = "true" ]; then
     source ./remove_tmp_files.sh
