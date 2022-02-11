130322220e3df16600973e0952b0d9b2e675dbd7
yoshihikosuzuki <ys.neoteny@gmail.com>
2022-02-11 19:34:52 +0900

    fix busco; improve config

--- template/10-contigs/template-phased/01-busco-hap1/remove_tmp_files.sh
+++ template/10-contigs/template-phased/01-busco-hap1/remove_tmp_files.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#SBATCH -J remove_tmp_files
-#SBATCH -o remove_tmp_files.log
-#SBATCH -p compute
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH -c 1
-#SBATCH --mem=1G
-#SBATCH -t 1:00:00
 shopt -s expand_aliases && source ~/.bashrc && set -e || exit 1
 
-rm -rf contigs.hap1.busco*/*/ busco_downloads/
+rm -rf contigs.hap1.busco/*/ busco_downloads/ augustus_config/
--- template/10-contigs/template-phased/01-busco-hap2/run_busco.sh
+++ template/10-contigs/template-phased/01-busco-hap2/run_busco.sh
@@ -4,25 +4,28 @@
 #SBATCH -p compute
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 16
+#SBATCH -c 128
 #SBATCH --mem=500G
 #SBATCH -t 48:00:00
 shopt -s expand_aliases && source ~/.bashrc && set -e || exit 1
 source ../../../config.sh
 
+ml ${_BUSCO}
+
 ASM=contigs.hap2.fasta
-N_THREADS=16
+BUSCO_DB=${BUSCO_DB}
+USE_AUGUSTUS=${USE_AUGUSTUS}
+N_CORE=${SLURM_CPUS_PER_TASK}
 
 OUT_PREFIX=$(basename ${ASM} .gz)
 OUT_DIR=${OUT_PREFIX%.*}.busco
+if [ "${USE_AUGUSTUS}" = "true" ]; then
+    AUGUSTUS_OPTION="--augustus"
+else
+    AUGUSTUS_OPTION=""
+fi
 
-ml ${_BUSCO}
-
-SHARED_ARGS="-f --update-data -c ${N_THREADS} -m genome -l ${BUSCO_DB} -i ${ASM}"
-## Case 1. Using Metaeuk for gene annotation
-#busco ${SHARED_ARGS} -o ${OUT_DIR}
-## Case 2. Using Augustus for gene annotation
-busco ${SHARED_ARGS} -o ${OUT_DIR}_augustus --augustus
+busco -f --update-data -c ${N_CORE} -m genome -l ${BUSCO_DB} -i ${ASM} -o ${OUT_DIR} ${AUGUSTUS_OPTION}
 
 if [ "$AUTO_DEL" = "true" ]; then
     source ./remove_tmp_files.sh
