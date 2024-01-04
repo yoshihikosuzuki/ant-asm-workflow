a9540590c9fecf23fc727d530a03e89677ca0bbb
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-01-04 14:56:53 +0900

    update

--- template/11-scaf/template-salsa/remove_tmp_files.sh
+++ template/11-scaf/template-salsa/remove_tmp_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s expand_aliases && source ~/.bashrc && set -e || exit 1
 
-rm *.bam *.bed
+rm *.bed
 cd *_salsa
 rm -rf *.bed splits/ aligned/*.sam aligned/dups.txt aligned/merged_sort.txt aligned/opt_dups.txt hic/temp.*
